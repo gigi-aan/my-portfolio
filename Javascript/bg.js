b7aa800e6ed1bfce99331766fd3413dbc6bb42ce
giya <giannealexa.bunag.cics@ust.edu.ph>
2025-05-31 00:18:30 +0800

    initial commit

---
+++ Javascript/bg.js
@@ -0,0 +1,33 @@
+new FinisherHeader({
+    "count": 12,
+    "size": {
+        "min": 77,
+        "max": 616,
+        "pulse": 0.3
+    },
+    "speed": {
+        "x": {
+        "min": 0,
+        "max": 1.4
+        },
+        "y": {
+        "min": 0,
+        "max": 1.2
+        }
+    },
+    "colors": {
+        "background": "#292929",
+        "particles": [
+        "#d4a6a4"
+        ]
+    },
+    "blending": "overlay",
+    "opacity": {
+        "center": 0.65,
+        "edge": 0
+    },
+    "skew": 0,
+    "shapes": [
+        "c"
+    ]
+    });
